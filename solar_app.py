7871ce549123443c361a5984141f722327b103e1
Declan Gordon <declan.gordon@zalando.ie>
2018-08-13 17:02:58 +0100

    Solar posoitioning algorithm wrapper module

---
+++ solar_app.py
@@ -0,0 +1,22 @@
+import sched, time, datetime
+import spa_mod
+
+scheduler = sched.scheduler(time.time, time.sleep)
+
+def periodic(scheduler, interval, action, actionargs=()):
+    scheduler.enter(interval, 1, periodic,
+                    (scheduler, interval, action, actionargs))
+    action(*actionargs)
+
+
+def write():
+    current_time = datetime.datetime.now()
+    position =  spa_mod.calculate_position(current_time.year,current_time.month,current_time.day,current_time.hour,current_time.minute,current_time.second,53.4,-6.14)
+    print('periodic ' + str(current_time) )
+    if position['status'] == 0:
+        print(position)
+
+periodic(scheduler, 5, write)
+
+scheduler.run()
+
