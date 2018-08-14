68214205f1da6c7f5cbc3c7a068f9a45ae5f9b89
Declan Gordon <declan.gordon@zalando.ie>
2018-08-14 08:11:59 +0100

    Sense and track functions

--- solar_app.py
+++ solar_app.py
@@ -8,15 +8,18 @@ def periodic(scheduler, interval, action, actionargs=()):
                     (scheduler, interval, action, actionargs))
     action(*actionargs)
 
+def sense():
+    print('sense')
 
-def write():
+def track():
     current_time = datetime.datetime.now()
     position =  spa_mod.calculate_position(current_time.year,current_time.month,current_time.day,current_time.hour,current_time.minute,current_time.second,53.4,-6.14)
     print('periodic ' + str(current_time) )
     if position['status'] == 0:
         print(position)
 
-periodic(scheduler, 5, write)
+periodic(scheduler, 10, track)
+periodic(scheduler, 2, sense)
 
 scheduler.run()
 
